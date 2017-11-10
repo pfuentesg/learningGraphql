d7ca322d04a21064f3e6ddad536f26264cfb1e2b
pfuentesg <pablofuentesg1@yahoo.es>
2017-11-10 19:46:13 +0100

    working on outh

--- graphqlTest/schema/index.js
+++ graphqlTest/schema/index.js
@@ -14,7 +14,24 @@ type Query {
 }
 type Mutation {
     createLink(url: String!, description: String!): Link
-  }
+    createUser(name: String!, authProvider: AuthProviderSignupData!): User
+}
+
+type User {
+    id: ID!
+    name: String!
+    email: String
+}
+
+input AuthProviderSignupData {
+    email: AUTH_PROVIDER_EMAIL
+}
+
+input AUTH_PROVIDER_EMAIL {
+    email: String!
+    password: String!
+}
+  
 `;
 
 // Generate the schema object from your types definition.
