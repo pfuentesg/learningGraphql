2fa3912ba7a1978fcc4c0c1e79384d9036a58422
pfuentesg <pablofuentesg1@yahoo.es>
2017-11-10 19:07:38 +0100

    query and mutation with db

--- graphqlTest/mongo-connector.js
+++ graphqlTest/mongo-connector.js
@@ -5,8 +5,6 @@ const MONGO_URL = 'mongodb://localhost:27017/hackernews';
 
 // 2
 module.exports = async () => {
-  const db = await MongoClient.connect(MONGO_URL)
-  .then(e=>console.log("connected to db"))
-  .catch(e=>console.log("imposible to connect"))
+  const db = await MongoClient.connect(MONGO_URL);
   return {Links: db.collection('links')};
 }
